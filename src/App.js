ff16f55f4c94e28747c049158502c841c069d2d5
emfancypants <e0559608@u.nus.edu>
2023-06-10 17:56:49 +0800

    initialise files

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+
+        <h1>
+            emilys personal portfolio!
+        </h1>
+
+    </div>
+  )
+}
+
+export default App
