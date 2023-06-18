bcf41c6b437c637161ce06785e409eac5ff67b62
emfancypants <e0559608@u.nus.edu>
2023-06-18 17:05:36 +0800

    edited assets

--- src/containers/home/Home.jsx
+++ src/containers/home/Home.jsx
@@ -2,26 +2,32 @@ import React from 'react';
 import './home.css'
 import emoji from '../../assets/smiley emoji.png'
 import { BsMouse } from "react-icons/bs";
+import room from '../../assets/room5.png'
 
 const Home = () => {
   return (
-    <div className='website__home'>
-      <div className='website__home-intro'>
-        <h4 id='start'>Hey there, my name is</h4>
-        <h1 id='name'>
-          Emily Kok
-          <img src= {emoji} alt="logo" />
-        </h1>
-        <h4 id='context'>
-          I am all about finding the middle ground <br/>between Data and Design
-        </h4>
+    <div className='website__home-whole'>
+      <div className='website__home'>
+        <div className='website__home-intro'>
+          <h4 id='start'>Hey there, my name is</h4>
+          <h1 id='name'>
+            Emily Kok
+            <img src= {emoji} alt="logo" />
+          </h1>
+          <h4 id='context'>
+            I am all about finding the middle ground <br/>between Data and Design
+          </h4>
+        </div>
+        <a className='website__home-scroll' href='#about'>
+          <button className='website__home-scroll-mouse' type='button'>
+            <BsMouse />
+            Scroll to find out more!
+          </button>
+        </a>
       </div>
-      <a className='website__home-scroll' href='#about'>
-        <button className='website__home-scroll-mouse' type='button'>
-          <BsMouse />
-          Scroll to find out more!
-        </button>
-      </a>
+      <div className='website__home-image'>
+        <img src={room} alt="" />
+      </div>
     </div>
   )
 }
